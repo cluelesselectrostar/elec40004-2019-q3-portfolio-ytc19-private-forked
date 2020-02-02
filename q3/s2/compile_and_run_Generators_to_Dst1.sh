a81ebdf6c18ea964411086efc3dea2cecb1a177f
ytc19 <ytc19@ic.ac.uk>
2020-02-02 15:43:18 +0000

    s2e5FixedDst1

---
+++ q3/s2/compile_and_run_Generators_to_Dst1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+g++ Chan.hpp
+g++ Chan_Dst1.hpp
+
+g++ Gen1.cpp -o Gen1_to_Dst1
+./Gen1_to_Dst1 > Gen1.sink1.txt
+
+g++ Gen2.cpp -o Gen2_to_Dst1
+./Gen2_to_Dst1 > Gen2.sink1.txt
